52345f88412eb73bfc71510ca5c0948f5e70c38c
0num4 <r.oonuma@matsuri-tech.com>
2023-11-26 16:31:53 +0900

    feat: init(poetry new)

---
+++ test.py
@@ -0,0 +1,14 @@
+from mjx import Observation, State, Action
+with open("2021080221gm-00a9-0000-85adf647&tw=0.json") as f:
+    lines = f.readlines()
+
+    for line in lines:
+        state = State(line)
+        print(state)
+
+        for cpp_obs, cpp_act in state._cpp_obj.past_decisions():
+            obs = Observation._from_cpp_obj(cpp_obs)
+            feature = obs.to_features(feature_name="mjx-small-v0")
+
+            action = Action._from_cpp_obj(cpp_act)
+            action_idx = action.to_idx()
